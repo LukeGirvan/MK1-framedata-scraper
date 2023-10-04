924a8246d699e853dab9e08417af99a62d20a1c4
LukeGirvan <126108451+LukeGirvan@users.noreply.github.com>
2023-10-04 18:04:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const readline = require('readline');
 const fs = require('fs');
 
 (async () => {
+  
   // Create a readline interface to get user input
   const rl = readline.createInterface({
     input: process.stdin,
@@ -11,31 +12,36 @@ const fs = require('fs');
 
   // Prompt the user for the character name
   rl.question('enter char name use - and not spaces: ', async (charname) => {
+    
     // Close the readline interface as we have the input we need
     rl.close();
 
+    // Launch the Puppeteer browser
     const browser = await puppeteer.launch({
       executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe",
-      headless: false,
-      defaultViewport: null,
-      devtools: true,
-      args: ["--window-size=1920,1080", "--window-position=1921,0"]
-    })
+      headless: false, // Run the browser in non-headless mode for debugging
+      defaultViewport: null, // Set the viewport to null for a custom window size
+      devtools: true, // Enable DevTools for debugging
+      args: ["--window-size=1920,1080", "--window-position=1921,0"] // Set browser window size and position
+    });
     const page = await browser.newPage();
-    // Navigate to the URL
+    
+    // Construct the URL based on the character name
     const base_url = `https://mk1.kombatakademy.com/move-list/?character=${charname}&kameo=cyrax&date=09-14-2023`;
     await page.goto(base_url);
 
+    // Click the button to switch to table view
     await page.click(
       '#post-142 > div > div > div.move-list-select-view > span:nth-child(3)'
     );
-    // Click the button to switch to table view
+   
+    // Wait for the table view selector to be visible
     await page.waitForSelector('#post-142 > div > div > div.move-list-select-view > span:nth-child(3)');
 
-    const arr = [];
-    const textArray = [];
-    // Function to prompt the user for input
+    const arr = []; // Array to store scraped data
+    const textArray = []; // Unused array for text data (you can remove it if not needed)
 
+    // Function to scrape data from the webpage
     async function scrapeData(page, selectorPrefix, textArray, arr) {
       for (let i = 1; i < 100; i++) {
         const row = await page.waitForSelector(
@@ -64,8 +70,6 @@ const fs = require('fs');
           node.textContent.trim()
         );
 
-        
-
         const button_data = {
           'OB': block_adv,
           'OH': hit_adv,
@@ -74,33 +78,35 @@ const fs = require('fs');
         };
         arr.push([button.trim(), button_data]);
         if (button.trim() === 'THROW or 13' || button.trim() === 'SSEX') {
-            break;
+            break; // Stop scraping when specific conditions are met
           }
       }
       
     }
 
+    // Function to close the browser
     async function end(browser) {
       await browser.close();
     }
-    // Stop the timer for the first part
+   
 
-    const selectorPrefix = 'basic-attacks';
-    const selectorPrefix2 = 'special-moves';
-    await scrapeData(page, selectorPrefix, textArray, arr);
-    await scrapeData(page, selectorPrefix2, textArray, arr);
-    await end(browser);
+    const selectorPrefix = 'basic-attacks'; // Selector for basic attacks
+    const selectorPrefix2 = 'special-moves'; // Selector for special moves
+    await scrapeData(page, selectorPrefix, textArray, arr); // Scrape data for basic attacks
+    await scrapeData(page, selectorPrefix2, textArray, arr); // Scrape data for special moves
+    await end(browser); // Close the browser
     const jsonOutput = {};
     arr.forEach(([key, value]) => {
     jsonOutput[key] = value;
     });
 
-    // Save the JSON to a file
+    // Save the scraped data as JSON to a file
     const jsonString = JSON.stringify(jsonOutput, null, 2); // Prettify the JSON with 2 spaces for indentation
     fs.writeFileSync(`${charname}_framedata.json`, jsonString);
     
-    console.log(arr);
+    console.log(arr); // Print the scraped data
 
   });
 })();
+
 
